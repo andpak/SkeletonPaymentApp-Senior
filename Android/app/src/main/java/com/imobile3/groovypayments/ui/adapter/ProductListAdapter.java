ba8267750aef35742788154823b9970ce656ba8e
Andy Pak <62770342+andpak@users.noreply.github.com>
2020-05-15 19:14:38 -0400

    [Android] Update product list style to match mock

--- Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/ui/adapter/ProductListAdapter.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.imobile3.groovypayments.R;
+import com.imobile3.groovypayments.data.enums.GroovyColor;
+import com.imobile3.groovypayments.data.enums.GroovyIcon;
 import com.imobile3.groovypayments.data.model.Product;
+import com.imobile3.groovypayments.rules.CurrencyRules;
 import com.imobile3.groovypayments.utils.StateListHelper;
 
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ProductListAdapter
         extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
@@ -25,6 +30,7 @@ public class ProductListAdapter
     private Context mContext;
     private AdapterCallback mCallbacks;
     private List<Product> mItems;
+    private CurrencyRules currencyRules;
 
     public interface AdapterCallback {
 
@@ -38,6 +44,7 @@ public class ProductListAdapter
         mContext = context;
         mCallbacks = callback;
         mItems = products;
+        currencyRules = new CurrencyRules();
     }
 
     @NotNull
@@ -55,6 +62,19 @@ public class ProductListAdapter
         holder.label.setText(item.getName());
         holder.label.setTextColor(
                 StateListHelper.getTextColorSelector(mContext, R.color.black_space));
+
+        holder.icon.setImageResource(GroovyIcon.fromId(item.getIconId()).drawableRes);
+        holder.icon.setBackgroundColor(mContext.getResources().getColor(
+                GroovyColor.fromId(item.getColorId()).colorRes));
+
+        String currencySymbol = currencyRules.getFormattedAmount(
+                item.getUnitPrice(), Locale.getDefault());
+        StringBuilder description = new StringBuilder();
+        description.append(currencySymbol)
+                .append(" | ")
+                .append(item.getNote());
+
+        holder.description.setText(description);
     }
 
     @Override
@@ -65,11 +85,15 @@ public class ProductListAdapter
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ViewGroup container;
         TextView label;
+        ImageView icon;
+        TextView description;
 
         ViewHolder(View itemView) {
             super(itemView);
             container = itemView.findViewById(R.id.container);
             label = itemView.findViewById(R.id.label);
+            icon = itemView.findViewById(R.id.icon);
+            description = itemView.findViewById(R.id.description);
             container.setOnClickListener(this);
         }
 
